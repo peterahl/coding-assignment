7974e78f541f1753c67183cb97e5839fe9bd415c
Peter Ahlström <pete.ahlstrom@gmail.com>
2019-10-18 10:26:31 +0200

    Added defer to mutex unlock

--- go/pkg/memstore/inmem.go
+++ go/pkg/memstore/inmem.go
@@ -28,45 +28,42 @@ type Store struct {
 
 func (s *Store) GetCmds() (error, []models.Message) {
 	s.RLock()
+	defer s.RUnlock()
 	data := make([]models.Message, 0, len(s.Cmds))
 	data = s.Cmds
-	s.RUnlock()
 	return nil, data
 }
 
 func (s *Store) GetMessages() (error, []models.Message) {
 	s.RLock()
+	defer s.RUnlock()
 	data := make([]models.Message, 0, len(s.Messages))
 	for _, value := range s.Messages {
 		data = append(data, value)
 	}
-	s.RUnlock()
 	sort.Sort(IdSorter(data))
 	return nil, data
 }
 
 func (s *Store) GetMessage(id uint64) (error, models.Message) {
 	s.RLock()
+	defer s.RUnlock()
 	if val, ok := s.Messages[id]; ok {
-		s.RUnlock()
 		return nil, val
-	} else {
-		s.RUnlock()
-		return fmt.Errorf("There is no message for id: %d", id), models.Message{}
 	}
+	return fmt.Errorf("There is no message for id: %d", id), models.Message{}
 }
 
 func (s *Store) UpdateMessage(msg models.Message) error {
 	id := msg.GetId()
 	s.Lock()
+	defer s.Unlock()
 	for key, _ := range s.Messages {
 		if key == id {
 			s.Messages[id] = msg
-			s.Unlock()
 			return nil
 		}
 	}
-	s.Unlock()
 	return fmt.Errorf("Msg does not exist")
 }
 
@@ -74,21 +71,21 @@ func (s *Store) NewMessage(msg models.Message) error {
 	id := atomic.AddUint64(&atomic_id, 1)
 	msg.Id = id
 	s.Lock()
+	defer s.Unlock()
 	s.Messages[id] = msg
-	s.Unlock()
 	return nil
 }
 
 func (s *Store) AddCommand(msg models.Message) error {
 	s.Lock()
+	defer s.Unlock()
 	s.Cmds = append(s.Cmds, msg)
-	s.Unlock()
 	return nil
 }
 
 func (s *Store) DeleteMessage(msg models.Message) error {
 	s.Lock()
+	defer s.Unlock()
 	delete(s.Messages, msg.Id)
-	s.Unlock()
 	return nil
 }
